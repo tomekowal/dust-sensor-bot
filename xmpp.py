a531f1c260625ab8633e9dc0fce22e29bd2d8cd2
Tomasz Kowal <tomasz.kowal@erlang-solutions.com>
2015-04-02 09:41:37 +0000

    Send data to graphite

--- xmpp.py
+++ xmpp.py
@@ -3,7 +3,11 @@ import serial
 
 from sleekxmpp import ClientXMPP
 from sleekxmpp.exceptions import IqError, IqTimeout
+import socket
+import time
 
+CARBON_SERVER = '31.172.186.54'
+CARBON_PORT = 2003
 
 class DustSensorBot(ClientXMPP):
     def __init__(self, jid, password, room, nick):
@@ -20,6 +24,13 @@ class DustSensorBot(ClientXMPP):
                                         wait=True)
         print retval
 
+def send_msg(message):
+    print 'sending message to graphite:\n%s' % message
+    sock = socket.socket()
+    sock.connect((CARBON_SERVER, CARBON_PORT))
+    sock.sendall(message)
+    sock.close()
+
 if __name__ == '__main__':
     # Ideally use optparse or argparse to get JID,
     # password, and log level.
@@ -30,11 +41,14 @@ if __name__ == '__main__':
     room = 'malopolska@muc.localhost'
     xmpp = DustSensorBot('dust_sensor@localhost', 'dust_sensor', room, 'dust_sensor')
     xmpp.register_plugin('xep_0045')
-    xmpp.connect(('31.172.186.54', 5222))
+    xmpp.connect((CARBON_SERVER, 5222))
     xmpp.process(block=False)
-    ser = serial.Serial('/dev/tty.usbmodem641', 9600) # Establish the connection on a specific port
+    ser = serial.Serial('/dev/ttyACM0', 9600) # Establish the connection on a specific port
 
     while True:
         dust_concentration = ser.readline().strip() # Read the newest output from the Arduino
         print dust_concentration, " particles per cubic feet"
         retval = xmpp.send_message(mto=room, mbody=str(dust_concentration), mtype="groupchat")
+        timestamp = int(time.time())
+        message = 'dust_sensor.concentration %s %d\n' % (dust_concentration, timestamp)
+        send_msg(message)
